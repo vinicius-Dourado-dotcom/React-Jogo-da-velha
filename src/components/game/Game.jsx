6005b79a964a477fcd57364fc17042a6c8d72873
vinicius-Dourado-dotcom <vidourado@hotmail.com>
2024-09-04 20:15:34 -0300

    Criado estados no gameOption

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,19 +1,17 @@
+import { useState } from 'react'
+
 import styles from './Game.module.css'
 
 import GameOption from '../gameOption/GameOption'
 
 function Game () {
+  const [gameState, setGameState] =useState(Array(9).fill(0))
+
   return (
-    <div className={styles.game}>
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
+    <div className={styles.game}> 
+    {
+      gameState.map((value, pos) => <GameOption key={`game-option-pos-${pos}`} status={value}/>)
+    }
     </div>
   )
 
